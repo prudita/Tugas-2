7d5b34c8da799b275537f5ac16b7d437d9dff9ee
Prudita Victory <prudita.victory@ui.ac.id>
2023-03-04 16:13:58 +0700

    tugas 3

--- study_tracker/urls.py
+++ study_tracker/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from study_tracker.views import show_tracker
+from study_tracker.views import show_tracker, create_assignment, show_xml, show_json
+
 
 app_name = 'study_tracker'
 
 urlpatterns = [
     path('', show_tracker, name='show_tracker'),
+    path('create', create_assignment, name='create_assignment'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+
 ]
--- study_tracker/views.py
+++ study_tracker/views.py
@@ -1,9 +1,38 @@
+from django.http import HttpResponseRedirect
+from study_tracker.forms import AssignmentForm
+from django.urls import reverse
+
 from django.shortcuts import render
 from study_tracker.models import Assignment
+from django.http import HttpResponse
+from django.core import serializers
+
+
 # Create your views here.
+def create_assignment(request):
+    form = AssignmentForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('study_tracker:show_tracker'))
+
+    context = {'form': form}
+    return render(request, "create_assignment.html", context)
+
+
 def show_tracker(request):
     assignment_data = Assignment.objects.all()
     context = {
-        'list_of_assignments' : assignment_data
+        'list_of_assignments': assignment_data
     }
     return render(request, "home.html", context)
+
+
+def show_xml(request):
+    data = Assignment.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+
+def show_json(request):
+    data = Assignment.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
