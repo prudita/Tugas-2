f497bf0cdbe160237f5b546cb30983893efd5902
Prudita <prudita.victory@ui.ac.id>
2023-03-12 20:50:26 +0700

    tugas 4

--- study_tracker/urls.py
+++ study_tracker/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from study_tracker.views import show_tracker, create_assignment, show_xml, show_json
+from study_tracker.views import register, login_user, logout_user
 
 
 app_name = 'study_tracker'
@@ -9,5 +10,15 @@ urlpatterns = [
     path('create', create_assignment, name='create_assignment'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+
+
+    path('login/', login_user, name='login'),
+
+    path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
+
+
+
 
 ]
--- study_tracker/views.py
+++ study_tracker/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponseRedirect
 from study_tracker.forms import AssignmentForm
 from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from django.shortcuts import render
 from study_tracker.models import Assignment
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -19,12 +27,16 @@ def create_assignment(request):
     context = {'form': form}
     return render(request, "create_assignment.html", context)
 
+...
+@login_required(login_url='/tracker/login/')
+
 
 def show_tracker(request):
     assignment_data = Assignment.objects.all()
     context = {
         'list_of_assignments': assignment_data,
-        'name': '',
+        'name': request.user.username,
+        'last_login': request.COOKIES['last_login'],
     }
     return render(request, "home.html", context)
 
@@ -37,3 +49,43 @@ def show_xml(request):
 def show_json(request):
     data = Assignment.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('study_tracker:login')
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)  # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(reverse("study_tracker:show_tracker"))  # membuat response
+            response.set_cookie('last_login',
+                                str(datetime.datetime.now()))  # membuat cookie last_login dan menambahkannya ke dalam response
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('study_tracker:login'))
+    response.delete_cookie('last_login')
+    return response
+
+
